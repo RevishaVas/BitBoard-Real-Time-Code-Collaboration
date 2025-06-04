a222711b90301f772288ce8cea5edb9e55e6a870
dhool19 <ayeshadhool24@gmail.com>
2025-06-04 16:25:32 +0200

    added dummy data

--- apps/frontend/src/main.jsx
+++ apps/frontend/src/main.jsx
@@ -1,17 +1,12 @@
 import React from "react";
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
 import store from "./redux/store.js";
-import { ThemeProvider } from "next-themes";
 import { Provider } from "react-redux";
+import { ThemeProvider } from "next-themes";
 import { RecoilRoot } from "recoil";
-
-
-import store from "./redux/store.js";
-import { Provider } from "react-redux";
-import { ThemeProvider } from "next-themes";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
