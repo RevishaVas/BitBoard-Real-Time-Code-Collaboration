2e676cbf9824df41e55956f605c8d6d3cd75c354
nayanasuresh13 <nayanasuresh390@gmail.com>
2025-06-11 15:18:48 +0200

    Add post comment feature with basic schema and route

--- apps/backend/comment-service/server.js
+++ apps/backend/comment-service/server.js
@@ -1,14 +1,26 @@
-require('dotenv').config(); 
+require('dotenv').config();
 
 const express = require('express');
+const mongoose = require('mongoose');
 const cors = require('cors');
 const http = require('http');
+const commentRoutes = require('./routes/commentRoutes');
+
 const app = express();
 const server = http.createServer(app);
 
 app.use(cors());
 app.use(express.json({ limit: '10mb' }));
+app.use('/api', commentRoutes);
 
 const PORT = process.env.PORT || 5002;
+const MONGO_URI = process.env.MONGO_URI;
 
-server.listen(PORT, () => console.log(`Server running at http://localhost:${PORT}`));
+mongoose.connect(MONGO_URI)
+  .then(() => {
+    console.log('MongoDB connected');
+    server.listen(PORT, () => console.log(`Server running at http://localhost:${PORT}`));
+  })
+  .catch(err => {
+    console.error('MongoDB connection error:', err);
+  });
